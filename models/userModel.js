f89836b4858dae4d32b2cf30c134e6d3e03d564b
Rutika Patne <Rutika.Patne@synechron.com>
2024-05-15 15:12:41 +0530

    userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -9,7 +9,7 @@ import dotenv from'dotenv'
 dotenv.config();
 
 const pool = new Pool({
-    user: 'postgres',
+    user: process.env.user,
     host: process.env.host,
     database: process.env.database,
     password: process.env.password,
