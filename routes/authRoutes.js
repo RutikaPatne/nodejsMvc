0935ef00d988fec3deaf37384ed17b1d46f5fe37
Rutika Patne <Rutika.Patne@synechron.com>
2024-05-15 15:09:24 +0530

    authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, loginFunction,authMiddleware, logoutFunction,profile, updateUser, deleteUser } from '../controllers/authController.mjs';
+import { register, loginFunction,authMiddleware, logoutFunction,profile, updateUser, deleteUser } from '../controllers/authController.js';
 
 
 const router = express.Router();
