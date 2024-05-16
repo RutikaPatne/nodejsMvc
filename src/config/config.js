3fb3c8c289050f7b69e1197ff171bf69b9b2e83a
Rutika Patne <Rutika.Patne@synechron.com>
2024-05-16 10:46:54 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const config = {
-    user: process.env.DB_USER || 'postgres', 
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME 
-    password: process.env.DB_PASSWORD 
-    port: process.env.DB_PORT || 5432 // PostgreSQL default port is 5432
+    user: process.env.DB_USER  ,
+    host: process.env.DB_HOST ,
+    database: process.env.DB_NAME, 
+    password: process.env.DB_PASSWORD ,
+    port: process.env.DB_PORT 
 };
 
 export default config;
