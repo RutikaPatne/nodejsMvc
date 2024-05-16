bf6d0512c4491bc1ac6c0aaf471b33cc675b60a3
Rutika Patne <Rutika.Patne@synechron.com>
2024-05-16 10:43:04 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,33 @@
+//crud 
+import { pool } from '../config/db.mjs'
 
+const updateUser = (req, res) => {
+    let id = req.query.id
+    const { username, password } = req.body
+
+    pool.query(`UPDATE usertable SET username=$1 , password=$2  where id=$3`, [username, password, id], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.json({
+            data: "updated successfully"
+        })
+    })
+}
+
+
+
+const deleteUser = (req, res) => {
+    let id = req.query.id;
+
+    pool.query(`DELETE FROM usertable WHERE id=$1`, [id], (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.json({
+            msg: `user with ${id} deleted successfully`
+        })
+    })
+}
+
+export{updateUser,deleteUser}
